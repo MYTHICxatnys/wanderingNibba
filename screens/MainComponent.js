c4d0c0d473bf1158f1c8e12a74ac6bf013aabb51
MythicXatnys <werli3d2@gmail.com>
2022-06-15 11:17:35 -0500

    Added About and Contact pages

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -7,12 +7,49 @@ import { createStackNavigator } from '@react-navigation/stack';
 import CampsiteInfoScreen from "./CampsiteInfoScreen";
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from './HomeScreen';
+import AboutScreen from './AboutScreen';
+import ContactScreen from './ContactScreen';
+
 
 const Drawer = createDrawerNavigator();
 
 const screenOptions = {
     headerTintColor: '#fff',
     headerStyle: { backgroundColor: '#5637DD' }
+}
+
+const ContactNavigator = () => {
+    const Stack = createStackNavigator();
+
+    return (
+        <Stack.Navigator
+            screenOptions={screenOptions}
+        >
+            <Stack.Screen
+                name='Contact'
+                component={ContactScreen}
+                options={{ title: 'Contact Us' }}
+            />
+
+        </Stack.Navigator>
+    )
+}
+
+const AboutNavigator = () => {
+    const Stack = createStackNavigator();
+
+    return (
+        <Stack.Navigator
+            screenOptions={screenOptions}
+        >
+            <Stack.Screen
+                name='About'
+                component={AboutScreen}
+                options={{ title: 'About' }}
+            />
+
+        </Stack.Navigator>
+    )
 }
 
 const HomeNavigator = () => {
@@ -83,6 +120,19 @@ const Main = () => {
                     component={DirectoryNavigator}
                     options={{ title: 'Directory' }}
                 />
+
+                <Drawer.Screen
+                    name='About'
+                    component={AboutNavigator}
+
+                />
+
+                <Drawer.Screen
+                    name='Contact'
+                    component={ContactNavigator}
+                    options={{ title: 'Contact Us' }}
+                />
+
             </Drawer.Navigator>
         </View>
     );
