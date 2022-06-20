73bd76964684c4e86c81dc884dd58e4a3b11e4d4
MythicXatnys <werli3d2@gmail.com>
2022-06-20 12:17:58 -0500

    Custom Side Drawer Added

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,15 +1,16 @@
 //import { useState } from "react";
 //import { CAMPSITES } from '../shared/campsites';
 import DirectoryScreen from './DirectoryScreen';
-import { Platform, View, StyleSheet } from "react-native";
+import { Platform, View, StyleSheet, Text, Image } from "react-native";
 import Constants from 'expo-constants';
 import { createStackNavigator } from '@react-navigation/stack';
 import CampsiteInfoScreen from "./CampsiteInfoScreen";
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import HomeScreen from './HomeScreen';
 import AboutScreen from './AboutScreen';
 import ContactScreen from './ContactScreen';
 import { Icon } from 'react-native-elements';
+import logo from '../assets/images/logo.png';
 
 
 const Drawer = createDrawerNavigator();
@@ -133,6 +134,23 @@ const DirectoryNavigator = () => {
     )
 }
 
+const CustomDrawerContent = (props) => {
+    return (
+        <DrawerContentScrollView {...props}>
+            <View style={styles.drawerHeader}>
+                <View style={{ flex: 1 }}>
+                    <Image source={logo} style={styles.drawerImage} />
+                </View>
+                <View style={{ flex: 2 }}>
+                    <Text style={styles.drawerHeaderText}>NuCamp</Text>
+                </View>
+            </View>
+
+            <DrawerItemList {...props} labelStyle={{ fontWeight: 'bold' }} />
+        </DrawerContentScrollView>
+    )
+}
+
 const Main = () => {
     //const [campsites, setCampsites] = useState(CAMPSITES);
     //const [selectedCampsiteId, setSelectedCampsiteId] = useState();
@@ -148,6 +166,7 @@ const Main = () => {
             <Drawer.Navigator
                 initialRouteName='Home'
                 drawerStyle={{ backgroundColor: '#CEC8FF' }}
+                drawerContent={CustomDrawerContent}
             >
                 <Drawer.Screen
                     name='Home'
@@ -224,6 +243,24 @@ const styles = StyleSheet.create({
         marginLeft: 10,
         color: '#fff',
         fontSize: 24
+    },
+    drawerHeader: {
+        backgroundColor: '#5637DD',
+        height: 140,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row'
+    },
+    drawerHeaderText: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    drawerImage: {
+        margin: 10,
+        height: 60,
+        width: 60
     }
-});
+})
 export default Main;
