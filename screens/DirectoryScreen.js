4b85dd56c6aca832c210937d2929a0ae8ff53f72
MythicXatnys <werli3d2@gmail.com>
2022-06-21 06:04:26 -0500

    Activity Indicators Added

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -1,8 +1,9 @@
 //import { useState } from "react";
-import { FlatList } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import { Avatar, ListItem, Tile } from "react-native-elements";
 import { useSelector } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from "../components/LoadingComponent";
 //import { CAMPSITES } from '../shared/campsites'
 
 const DirectoryScreen = ({ navigation }) => {
@@ -10,6 +11,17 @@ const DirectoryScreen = ({ navigation }) => {
 
     const campsites = useSelector((state) => state.campsites);
 
+    if (campsites.isLoading) {
+        return <Loading />
+    }
+
+    if (campsites.errMess) {
+        return (
+            <View>
+                <Text>{campsites.errMess}</Text>
+            </View>
+        );
+    }
 
     const renderDirectoryItem = ({ item: campsite }) => {
         return (
